a4b37b5545d06a062377f5714b3cea4dc55925b1
saniazt <saniazt@gmail.com>
2022-09-27 18:32:58 +0300

    27.09.2022 18:32

--- src/test/java/Vector2DTest.java
+++ src/test/java/Vector2DTest.java
@@ -1,23 +1,23 @@
 import jUnit1.Vector2D;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class Vector2DTest {
+    private final double EPS = 1e-9; //1e-9 = 0.000......01
+    private static Vector2D v1;
+    @BeforeClass //выполняется один раз, STATIC
+    public static void createNewVector(){
+        v1 = new Vector2D();
+    }
     @Test
     public void newVectorShouldHaveZeroWeight(){
-        Vector2D v1 =new Vector2D(); //action - создали новый вектор
-
-        Assert.assertEquals(0,v1.length(),1e-9);//assertion
-        //1e-9 = 0.000......01
+        Assert.assertEquals(0,v1.length(),EPS);//assertion
     }
     @Test
-    public void newVectorShouldHaveZeroX(){
-        Vector2D v1 = new Vector2D();
-        Assert.assertEquals(0,v1.getX(),1e-9);
-    }
-    @Test
-    public void newVectorShouldHaveZeroY(){
-        Vector2D v1 = new Vector2D();
-        Assert.assertEquals(0,v1.getY(),1e-9);
+    public void newVectorShouldHaveZeroXAndZeroY(){ // но так делать не стоит, лучше отдельно
+        Assert.assertEquals(0,v1.getX(),EPS);
+        Assert.assertEquals(0,v1.getY(),EPS);
     }
 }
